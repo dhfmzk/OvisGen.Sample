c1595aea046e2169b7faa13a7cd46b1fccce158d
Orca <rlatkdgus500@gmail.com>
2025-01-14 08:35:21 +0900

    💩 Dummy for samples

---
+++ Assets/Scripts/DummyMonoBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OvisSampleUnity
+{
+    public class DummyMonoBehaviour : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        
+        public override string ToString()
+        {
+            return "DummyMonoBehaviour";
+        }
+    }
+}
---
+++ Assets/Scripts/DummyMonoBehaviour2.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace OvisSampleUnity
+{
+    public class DummyMonoBehaviour2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        
+        public override string ToString()
+        {
+            return "DummyMonoBehaviour2";
+        }
+    }
+}
